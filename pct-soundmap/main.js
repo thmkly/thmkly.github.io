2a69281363522675ccb860f5f3f75b8eb16abc38
thmkly <49326127+thmkly@users.noreply.github.com>
2025-08-23 11:23:52 -0700

    Update main.js

--- pct-soundmap/main.js
+++ pct-soundmap/main.js
@@ -1,7 +1,9 @@
 // main.js
 const audioController = new AudioController();
 const atmosphereController = new AtmosphereController();
+const uiController = new UIController();
 
 // Expose to global scope (so classes can talk to each other)
 window.audioController = audioController;
 window.atmosphereController = atmosphereController;
+window.uiController = uiController;
