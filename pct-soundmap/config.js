13dbed65b7675def85b156b766ec23c1e1009a5a
thmkly <49326127+thmkly@users.noreply.github.com>
2025-10-17 21:09:07 -0700

    Update config.js

--- pct-soundmap/config.js
+++ pct-soundmap/config.js
@@ -3,7 +3,7 @@
       MAPBOX_TOKEN: window.MAPBOX_CONFIG?.token || 'pk.eyJ1IjoidGhta2x5IiwiYSI6ImNseXVyMjhueDA3YTQybW9mcHJrZGJ3YnEifQ.Nv-LsNg5eKIE6SeOVVJpYg',
       GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwEvzIyQTFTwmPqU0xIoty1iuBd3xxBeLFYHljXrXCYSkxJEsWKPKYJKti9rBqGx6-0/exec',
       DEFAULT_CENTER: [-122.50276, 41.31727],
-      DEFAULT_CENTER_MOBILE: [-118.70077196971125, 41.42702394449324],
+      DEFAULT_CENTER_MOBILE: [-119.80462182339255, 41.37182150227608],
       getDefaultZoom: () => {
         // Mobile gets fixed zoom to account for Ko-fi widget
         if (window.innerWidth <= 768) {
