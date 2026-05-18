3c022782f873a11b46e5cf2ceaf6693481024cb2
thmkly <49326127+thmkly@users.noreply.github.com>
2026-05-17 19:21:09 -0700

    Add files via upload

--- pct-soundmap/soundmap2026/audiocontroller.js
+++ pct-soundmap/soundmap2026/audiocontroller.js
@@ -98,9 +98,9 @@ class AudioController {
       album: 'a sound map of the pacific crest trail, 2023',
       artwork: [
         {
-          src: 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512"><rect width="512" height="512" fill="%23f5f0eb"/><text x="50%25" y="50%25" font-family="helvetica" font-size="48" fill="%23888" text-anchor="middle" dominant-baseline="middle">stereo</text></svg>',
+          src: 'https://www.thomasmkelly.com/favicon-sierra.png',
           sizes: '512x512',
-          type: 'image/svg+xml'
+          type: 'image/png'
         }
       ]
     });
