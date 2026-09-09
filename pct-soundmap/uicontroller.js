516b63acadf770b3ddc6224d22843cb9a32ce570
thmkly <49326127+thmkly@users.noreply.github.com>
2026-09-09 16:03:44 -0700

    Add files via upload

--- pct-soundmap/uicontroller.js
+++ pct-soundmap/uicontroller.js
@@ -8,6 +8,7 @@ class UIController {
       this.clusterPlaylist = null;
       this.isMobile = this._detectMobile();
       document.body.classList.toggle('is-mobile', this.isMobile);
+      document.documentElement.classList.remove('is-mobile');
       
       // Cache DOM references
       this.playlist = document.getElementById('playlist');
